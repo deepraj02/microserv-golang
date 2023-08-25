098cd295f6050f84f3eb421b27e6ac40677595ec
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-25 21:15:46 +0530

    📦 FEAT: broker service --comp

--- broker-serv/cmd/apis/main.go
+++ broker-serv/cmd/apis/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
 
 func main() {
-	fmt.Println("Hello GO")
+	app := Config{}
+	log.Printf("Starting broker on port %s\n", webPort)
+
+	//define the http server
+	serv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	//Start the server
+	err := serv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
 }
